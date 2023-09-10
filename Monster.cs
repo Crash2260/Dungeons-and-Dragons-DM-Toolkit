0c09f5646107ddba561c11b2a00a6c159555db39
Crash2260 <102874651+Crash2260@users.noreply.github.com>
2023-09-09 22:10:18 -0400

    Made two classes. Pretty basic so far. One for monsters and one for players.

---
+++ Monster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+public class Monster
+{
+	//Create a spot to hold character values of each entity
+
+	
+	private string name;
+	private string size;
+	private string type;
+	private string alignment;
+	private int armorClass;
+	private int hitPoints;
+	private string speeds;
+	private int strenght;
+	private int dexterity;
+	private int constitution;
+	private int intellegence;
+	private int wisdom;
+	private int charisma;
+	private string savingThrows;
+	private string skills;
+	private string wri;
+	private string senses;
+	private string languages;
+	private string additional;
+	private string bookFoundIn;
+
+	public Monster()
+	{
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Player
+{
+	//Create basic player stats from character sheet
+	private string name;
+	private int level;
+	private int xp;
+	private int gold;
+	private int silver;
+	private int copper;
+	private bool inspiration;
+	private int proficiencyBonus;
+	private int armorClass;
+	private int speed;
+	private int hp;
+	private int deathSaveSucesses;
+	private int deathSaveFailures;
+	private int passiveWisdom;
+	private int strength;
+    private int strengthBonus;
+    private int dexterity;
+    private int dexterityBonus;
+    private int constitution;
+    private int constitutionBonus;
+    private int intellegence;
+    private int intellegenceBonus;
+    private int wisdom;
+    private int wisdomBonus;
+    private int charisma;
+    private int charismaBonus;
+
+
+
+
+    public Player()
+	{
+
+	}
+}
